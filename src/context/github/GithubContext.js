675bcd69066f8e88a8f6c961e0e75ddf7c5ba118
Ranjan Verma <verma.ranjan62@gmail.com>
2022-08-06 22:46:24 +0530

    Clear Users, AlertContext

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -33,9 +33,15 @@ export const GithubProvider = ({ children }) => {
   //Set Loading
   const setLoading = () => dispatch({ type: 'SET_LOADING' });
 
+  const clearUsers = () => dispatch({ type: 'CLEAR_USERS' });
   return (
     <GithubContext.Provider
-      value={{ users: state.users, loading: state.loading, searchUsers }}
+      value={{
+        users: state.users,
+        loading: state.loading,
+        searchUsers,
+        clearUsers,
+      }}
     >
       {children}
     </GithubContext.Provider>
