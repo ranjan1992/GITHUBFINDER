32d6901570fba7bbc733e9e0f86ad3bc08685811
Ranjan Verma <verma.ranjan62@gmail.com>
2022-08-06 17:12:26 +0530

    Navbar and Footer Components Made

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Footer from './components/layout/Footer';
+import Navbar from './components/layout/Navbar';
 function App() {
-  return <div className="App"></div>;
-}
+  return (
+    <Router>
+      <div className="flex flex-col justify-between h-screen">
+        <Navbar />
 
+        <main className="container mx-auto px-3 pb-12">Content</main>
+        <Footer />
+      </div>
+    </Router>
+  );
+}
 export default App;
